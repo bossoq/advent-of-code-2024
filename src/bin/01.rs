06af78e5b5dd5cc98c9e2a33d9e98d0bec1c6fdf
kittipos.w <kittipos.w@kasikornline.com>
2024-12-01 16:28:39 +0700

    Add day 1 solution

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,11 +1,40 @@
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let mut group_1: Vec<i32> = Vec::new();
+    let mut group_2: Vec<i32> = Vec::new();
+    input
+        .lines()
+        .map(|line| line.split("   "))
+        .for_each(|mut group| {
+            group_1.push(group.next().unwrap().parse().unwrap());
+            group_2.push(group.next().unwrap().parse().unwrap());
+        });
+    group_1.sort();
+    group_2.sort();
+    let mut result = 0;
+    for i in 0..group_1.len() {
+        result += (group_1[i] - group_2[i]).abs();
+    }
+    Some(result as u32)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut group_1: Vec<i32> = Vec::new();
+    let mut group_2: Vec<i32> = Vec::new();
+    input
+        .lines()
+        .map(|line| line.split("   "))
+        .for_each(|mut group| {
+            group_1.push(group.next().unwrap().parse().unwrap());
+            group_2.push(group.next().unwrap().parse().unwrap());
+        });
+    let mut result = 0;
+    for num in group_1.iter() {
+        let cnt = group_2.clone().into_iter().filter(|x| x == num).count() as i32;
+        result += num * cnt;
+    }
+    Some(result as u32)
 }
 
 #[cfg(test)]
@@ -15,12 +44,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(11));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }
